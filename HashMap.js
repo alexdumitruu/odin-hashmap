f4cf748292f173d2930d46ccd6cfc4746a1a92b6
alexdumitruu <alex.dumitru.g@gmail.com>
2025-10-04 16:04:59 +0300

    started

---
+++ HashMap.js
@@ -0,0 +1,19 @@
+function HashMap() {
+  const loadFactor = 0.75;
+  const capacity = 16;
+
+  function hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+    }
+
+    return hashCode;
+  }
+
+  function set(key, value) {
+    
+  }
+}
